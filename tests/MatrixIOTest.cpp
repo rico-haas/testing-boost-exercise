044d97e9d5f57fd14380b2a0bddabefd773e1c1a
Rico Haas <git.rico-haas@gmx.de>
2026-02-04 08:00:35 +0100

    style: apply formatter

--- tests/MatrixIOTest.cpp
+++ tests/MatrixIOTest.cpp
@@ -6,13 +6,13 @@ BOOST_AUTO_TEST_SUITE(MatrixIoTests)
 
 BOOST_AUTO_TEST_CASE(OpenData)
 {
-    Eigen::MatrixXd expected_matrix(3, 3);
-    expected_matrix << 5, 3, 8,
-        4, 6, 7,
-        1, 1, 1;
-    
-    Eigen::MatrixXd actual_matrix = matrixIO::openData("open_data_test_matrix.csv", 3);
-    BOOST_TEST(actual_matrix.isApprox(expected_matrix));
+  Eigen::MatrixXd expected_matrix(3, 3);
+  expected_matrix << 5, 3, 8,
+      4, 6, 7,
+      1, 1, 1;
+
+  Eigen::MatrixXd actual_matrix = matrixIO::openData("open_data_test_matrix.csv", 3);
+  BOOST_TEST(actual_matrix.isApprox(expected_matrix));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
